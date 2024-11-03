15b878f1d8db6ba4409d6ea5a566640c6e2511d6
Saifee Rahman Saif <saifeer1019@gmail.com>
2024-11-03 21:52:18 +0600

    edit

--- src/app/[slug]/page.js
+++ src/app/[slug]/page.js
@@ -1,5 +1,5 @@
 // src/app/[slug]/page.js
 export default function Page({ params }) {
-    const { slug } = params.slug; // Await the params object
-    return <div>{slug}</div>;
+    const  slug2  = params.slug; // Await the params object
+    return <div>It is {slug2}</div>;
 }
